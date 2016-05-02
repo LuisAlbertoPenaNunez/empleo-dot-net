daf467141b884d07024099f64f6f0cbb23dbf98a
Luis Alberto Pena Nunez <luis.pena.nunez.developer@gmail.com>
2016-05-02 15:01:55 -0600

    Introduced handling to the get request for JobControllerAPI

--- EmpleadoDotNet.MobileAPI/Controllers/JobOpportunityController.cs
+++ EmpleadoDotNet.MobileAPI/Controllers/JobOpportunityController.cs
@@ -1,22 +1,55 @@
-﻿using System;
+﻿using EmpleoDotNet.Core.Domain;
+using EmpleoDotNet.Repository.Contracts;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using EmpleoDotNet.Core.Dto;
 
 namespace EmpleadoDotNet.MobileAPI.Controllers
 {
     public class JobOpportunityController : ApiController
     {
-        public HttpResponseMessage Get()
-        {
-            return Request.CreateResponse(HttpStatusCode.OK, "Hi");
-        }
+        private readonly IJobOpportunityRepository _jobOpportunityRepository;
         
-        public HttpResponseMessage Get(int id)
+        public JobOpportunityController(IJobOpportunityRepository jobOpportunityRepository)
         {
-            return Request.CreateResponse(HttpStatusCode.OK, "Hi");
+            _jobOpportunityRepository = jobOpportunityRepository;
+        }
+
+        [Route("cardJobs")]
+        public HttpResponseMessage Get([FromUri]JobOpportunityPagingParameter parameter)
+        {
+            var jobs = _jobOpportunityRepository.GetAllJobOpportunitiesPagedByFilters(parameter);
+
+            if (jobs == null || jobs.Count == 0)
+                return Request.CreateResponse(HttpStatusCode.NotFound);
+
+            return Request.CreateResponse(HttpStatusCode.OK, jobs);
+        }
+
+        [Route("cardJobs/detail")]
+        public HttpResponseMessage Get(string id)
+        {
+            var specifiedId = GetIdFromTitle(id);
+
+            var jobs = _jobOpportunityRepository.GetJobOpportunityById(specifiedId);
+
+            return jobs == null ? Request.CreateResponse(HttpStatusCode.NotFound) :
+                Request.CreateResponse(HttpStatusCode.OK, "Hi");
+        }
+
+        private static int GetIdFromTitle(string title)
+        {
+            int id;
+            var url = title.Split('-');
+
+            if (string.IsNullOrEmpty(title) || url.Length == 0 || !int.TryParse(url[0], out id))
+                return 0;
+
+            return id;
         }
     }
 }
