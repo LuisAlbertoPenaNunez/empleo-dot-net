f383a50feb077c93f70edd91e8c33b4f544ab96a
Luis Alberto Pena Nunez <luis.pena.nunez.developer@gmail.com>
2016-05-02 13:36:27 -0600

    Created Mobile API solution

---
+++ EmpleadoDotNet.MobileAPI/Controllers/JobOpportunityController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace EmpleadoDotNet.MobileAPI.Controllers
+{
+    public class JobOpportunityController : ApiController
+    {
+        public HttpResponseMessage Get()
+        {
+            return Request.CreateResponse(HttpStatusCode.OK, "Hi");
+        }
+        
+        public HttpResponseMessage Get(int id)
+        {
+            return Request.CreateResponse(HttpStatusCode.OK, "Hi");
+        }
+    }
+}
---
+++ EmpleadoDotNet.MobileAPI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EmpleadoDotNet.MobileAPI.WebApiApplication" Language="C#" %>
